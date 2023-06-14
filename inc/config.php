7fd2cbcb31e21b43cd9df7cae22cdaf94434b044
Asmeg <repulsive7@gmail.com>
2023-06-14 12:01:12 +0900

    20230614

--- inc/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Azure Database configuration
-const DB_HOST = 'postasmeg.postgres.database.azure.com';
-const DB_PORT = '5432';
-const DB_NAME = 'employees';
-const DB_USER = 'haku';
-const DB_PASSWORD = 'DevOps79';
